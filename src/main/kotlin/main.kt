7f9f8c2a91a565492a0571fce26d24d8930d8a02
saifi369 <chsaif369@gmail.com>
2021-04-25 17:25:38 +0500

    main file updated

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,28 +3,34 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.runBlocking
 
-fun main() = runBlocking{
+fun main() =
+    runBlocking {
+        joinAll(
+            async { coroutineOne() },
+            async { coroutineTwo() }
+        )
+    }
 
-    println("Main starts")
-
-    joinAll(
-        async { coroutineOne() },
-        async { coroutineTwo() }
-    )
-
-    println("Main ends")
-
-
-}
-
-suspend fun coroutineOne(){
+suspend fun coroutineOne() {
     println("Coroutine one is started")
-    delay(1000)
+    delay(3000)
     println("Coroutine one is complete")
 }
 
-suspend fun coroutineTwo(){
+suspend fun coroutineTwo() {
     println("Coroutine two is started")
     delay(2000)
     println("Coroutine two is complete")
+}
+
+fun routineOne() {
+    println("Routine one is started")
+    Thread.sleep(3000)
+    println("Routine one is complete")
+}
+
+fun routineTwo() {
+    println("Routine two is started")
+    Thread.sleep(2000)
+    println("Routine two is complete")
 }
